24552b272ccc3cdbd94470398dbb2b5044b86098
rupert-ong <rupert.ong@gmail.com>
2021-03-26 18:52:58 -0400

    Add core-js and plugin-transform-runtime to babel

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
-const App = () => <h1>React TypeScript Starter Kit</h1>;
+import { useEffect } from "react";
+
+const App = () => {
+  useEffect(() => {
+    function test() {
+      return new Promise<boolean>((resolve) => {
+        setTimeout(() => resolve(true), 1000);
+      });
+    }
+
+    test().then((response) => console.log(response));
+  }, []);
+  return <h1>React TypeScript Starter Kit</h1>;
+};
 
 export default App;
