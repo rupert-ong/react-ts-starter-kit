c32c9a68fe4599d5e747048defc2b6a4206651ba
rupert-ong <rupert.ong@gmail.com>
2021-03-26 18:26:59 -0400

    Change webpack config to JS

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "/src/index.tsx"),
+  entry: path.resolve(__dirname, "./src/index.tsx"),
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
@@ -22,7 +22,7 @@ module.exports = {
   mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "/src/index.html"),
+      template: path.resolve(__dirname, "./src/index.html"),
     }),
   ],
 };
