59dce4335a88bcf2c4c7a38a291d97899358d40a
rupert-ong <rupert.ong@gmail.com>
2021-04-03 22:22:49 -0400

    Add image declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,9 +1,12 @@
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+declare module '*.gif';
+declare module '*.jpg';
+declare module '*.jpeg';
 declare module '*.png';
 declare module '*.svg' {
   const content: string;
-  export default content;
-}
-declare module '*.css' {
-  const content: Record<string, string>;
   export default content;
 }
